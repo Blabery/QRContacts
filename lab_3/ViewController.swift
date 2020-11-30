158c5d4af517f2f420008fb2124373b938b91c07
Vladislav Yakubets <7965474@gmail.com>
2020-11-30 14:52:53 +0300

    Initial Commit

---
+++ lab_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab_3
+//
+//  Created by Владислав Якубец on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
